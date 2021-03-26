03808c1ffb627a2516f294909c0b2bd328d49729
Ryan Andrews <rcandrews44@gmail.com>
2021-03-25 20:33:16 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Follow along with the examples here
+function doNothing() {}
+
+function sayHello() {
+    console.log('Hello!');
+  }
+
+sayHello();
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+  }
+
+sayHelloToIsabel()
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+  }
+
+sayHelloToSofia()
+
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+  }
+
+  sayHelloToIsabel();
+  sayHelloToSofia();
+  sayHelloToBrendan();
+
+function say(greeting, firstName) {
+    console.log(`${greeting}, ${firstName}!`);
+  }
+
+  say("Goodbye", "Julio");
+
+  function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`
+  }
+
+function add(x, y) {
+    return x + y;
+  }
+
+  console.log(add(1,2));
+
+  function say(greeting, firstName) {
+    console.log('I was called!');
+    return `${greeting}, ${firstName}!`;
+  }
+  say()
